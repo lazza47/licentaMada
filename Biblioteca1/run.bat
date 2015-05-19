82dbf99644fa0c4af9835a5c6c4884dd4841cd01
Lazza <Lazza@Lazza-PC>
2015-05-19 14:27:36 +0300

    namechanged

--- Biblioteca1/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Start program
-pause
-java -jar "%~dp0\build\jar\Biblioteca.jar"
-pause
--- Biblioteca1/src/com/lazarescu/licenta/biblioteca/MeniuMembruInregistrat.java
+++
@@ -1,9 +0,0 @@
-package com.lazarescu.licenta.biblioteca;
-
-public class MeniuMembruInregistrat extends Meniu {
-
-	public static void meniu() {
-		FramePrincipalMembruInregistrat frame = new FramePrincipalMembruInregistrat();
-		frame.setVisible(true);
-	}
-}
