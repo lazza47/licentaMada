fa578090abe7e68c08d46b433a144bf580cafe67
Lazza <Lazza@Lazza-PC>
2015-05-19 14:49:30 +0300

    d

--- Biblioteca1/src/com/lazarescu/licenta/biblioteca/Meniu.java
+++
@@ -1,6 +0,0 @@
-package com.lazarescu.licenta.biblioteca;
-
-public abstract class Meniu {
-	protected static Utilizator utilizator;
-	protected static Biblioteca biblioteca = Biblioteca.getInstance();
-}
--- Biblioteca1/src/com/lazarescu/licenta/biblioteca/MeniuGuest.java
+++
@@ -1,9 +0,0 @@
-package com.lazarescu.licenta.biblioteca;
-
-public class MeniuGuest extends Meniu {
-
-	public static void meniu() {
-		FramePrincipalGuest framePrincipalGuest = new FramePrincipalGuest();
-		framePrincipalGuest.setVisible(true);
-	}
-}
